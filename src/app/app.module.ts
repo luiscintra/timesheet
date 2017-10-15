165ff2f63f7e169dde0aef813489497ec00efe29
Luis Oliveira <luis.oliveira@e-core.com>
2017-10-15 16:25:30 -0200

    Basic Routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,47 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule }   from '@angular/router';
 
 import { AppComponent } from './app.component';
-//import { TimePeriod } from './time-period';
+import { MainComponent } from './main-component/main.component';
+import { WeekComponent } from './week-component/week.component';
+import { MonthComponent } from './month-component/month.component';
+import { YearComponent } from './year-component/year.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    WeekComponent,
+    MonthComponent,
+    YearComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {
+        path: '',
+        redirectTo: '/main',
+        pathMatch: 'full'
+      },
+      {
+        path: 'main',
+        component: MainComponent
+      },
+      {
+        path: 'week',
+        component: WeekComponent
+      },
+      {
+        path: 'month',
+        component: MonthComponent
+      },
+      {
+        path: 'year',
+        component: YearComponent
+      }
+
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/year-component/year.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { TimePeriod } from '../time-period'
+
+@Component({
+ selector: 'year',
+ templateUrl: './year.component.html'
+})
+export class YearComponent {
+
+    tm : TimePeriod = new TimePeriod('08:45', '11:45', '03:00', null);
+    
+    startTime : string = this.tm.startTime;
+    finishTime : string = this.tm.finishTime;
+    totalTime : string = this.tm.totalTime;
+  
+    todayEntries : TimePeriod[] = [
+      new TimePeriod('08:45', '11:45', '03:00', null),
+      new TimePeriod('12:00', '13:00', '01:00', null),
+      new TimePeriod('13:30', '14:30', '01:00', null),
+      new TimePeriod('15:00', '17:00', '02:00', null),
+      new TimePeriod('20:00', '23:45', '03:45', null)
+    ];
+ 
+}
