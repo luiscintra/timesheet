4cc4de0af4d58d5f31f7d8fe9f8f20458eac6107
Luis Oliveira <luis.oliveira@e-core.com>
2017-10-15 14:54:31 -0200

    Timesheet app using Angular 4

---
+++ src/app/app.module.ts
@@ -0,0 +1,17 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+//import { TimePeriod } from './time-period';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/time-period.ts
@@ -0,0 +1,9 @@
+export class TimePeriod {
+    constructor(
+        public startTime : string,
+        public finishTime : string,
+        public totalTime : string,
+        public description : string) {
+
+    }
+}
