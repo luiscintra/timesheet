07bfa6d3e8321e84a942dd13e21b20d773d13889
Luis Oliveira <luis.oliveira@e-core.com>
2017-11-01 00:19:20 -0200

    Adding time with seconds and reload every one second

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,20 +11,36 @@ export class AppComponent {
   title = 'Timesheet';
   today: Date;
 
+  start: Date = new Date("2017-11-01T00:13:00");
+
+  one_second: number = 1000;
+  one_minute: number = this.one_second * 60;
+  one_hour: number = this.one_minute * 60;
+  one_day: number = this.one_hour * 24;
+  one_month: number = this.one_day * 30;
+
   constructor(){
 
     this.today = new Date();
+
+    setInterval(() => {   
+
+      this.today = new Date();
+
+      let now: number = new Date().getTime();
+
+      console.log("Ms: " + Math.abs (now - this.start.getTime()));
+
+      let sec: number = Math.round ((now - this.start.getTime()) / this.one_second);
+
+      console.log("Seconds: " + sec);
+      console.log("Minutes: " + Math.round ((now - this.start.getTime()) / this.one_minute));
+      console.log("Hours: " + Math.round ((now - this.start.getTime()) / this.one_hour));
+      console.log("Days: " + Math.round ((now - this.start.getTime()) / this.one_day));
+
+
+    }, 1000);
   
-  }
-  
-  addTime() : void {
-
-    // As the date object does not change, change detection does not happen
-    this.today.setMinutes(this.today.getMinutes() + 10);
-
-    // Hence, the simpler solution is to create another date object
-    this.today = new Date(this.today.toISOString());
-
   }
 
 }
