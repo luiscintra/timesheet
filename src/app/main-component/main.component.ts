cc22ba5a59db73f3041415dc24b018534335e82a
Luis Oliveira <luis.oliveira@e-core.com>
2017-11-04 15:46:28 -0200

    Now using Date instead of String in the model

--- src/app/main-component/main.component.ts
+++ src/app/main-component/main.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { TimeRecord } from '../model/time-record'
+import { TimePeriod } from '../model/time-period'
 
 @Component({
  selector: 'main',
@@ -7,18 +8,18 @@ import { TimeRecord } from '../model/time-record'
 })
 export class MainComponent {
 
-    tm : TimeRecord = new TimeRecord('08:45', '11:45', '03:00', null);
+    tm : TimeRecord = new TimeRecord(new Date()/*'08:45'*/, new Date()/*'11:45'*/, new TimePeriod(new Date(2017, 11, 5, 3, 4, 0).getTime(), new Date(2017, 11, 5, 5, 10, 0).getTime()), null);
     
-    startTime : string = this.tm.startTime;
-    finishTime : string = this.tm.finishTime;
-    totalTime : string = this.tm.totalTime;
+    startTime : Date = this.tm.startTime;
+    finishTime : Date = this.tm.finishTime;
+    totalTime : TimePeriod = this.tm.totalTime;
   
     todayEntries : TimeRecord[] = [
-      new TimeRecord('08:45', '11:45', '03:00', null),
-      new TimeRecord('12:00', '13:00', '01:00', null),
-      new TimeRecord('13:30', '14:30', '01:00', null),
-      new TimeRecord('15:00', '17:00', '02:00', null),
-      new TimeRecord('20:00', '23:45', '03:45', null)
+      new TimeRecord(/*'08:45'*/new Date(), /*'11:45'*/new Date(), new TimePeriod(new Date(2017, 11, 5, 3, 4, 0).getTime(), new Date(2017, 11, 5, 5, 10, 0).getTime()), null),
+      new TimeRecord(/*'12:00'*/new Date(), /*'13:00'*/new Date(), new TimePeriod(new Date(2017, 11, 5, 3, 4, 0).getTime(), new Date(2017, 11, 5, 5, 10, 0).getTime()), null),
+      new TimeRecord(/*'13:30'*/new Date(), /*'14:30'*/new Date(), new TimePeriod(new Date(2017, 11, 5, 3, 4, 0).getTime(), new Date(2017, 11, 5, 5, 10, 0).getTime()), null),
+      new TimeRecord(/*'15:00'*/new Date(), /*'17:00'*/new Date(), new TimePeriod(new Date(2017, 11, 5, 3, 4, 0).getTime(), new Date(2017, 11, 5, 5, 10, 0).getTime()), null),
+      new TimeRecord(/*'20:00'*/new Date(), /*'23:45'*/new Date(), new TimePeriod(new Date(2017, 11, 5, 3, 4, 0).getTime(), new Date(2017, 11, 5, 5, 10, 0).getTime()), null)
     ];
  
 }
--- src/app/model/time-record.ts
+++ src/app/model/time-record.ts
@@ -1,8 +1,10 @@
+import {TimePeriod} from './time-period'
+
 export class TimeRecord {
     constructor(
-        public startTime : string,
-        public finishTime : string,
-        public totalTime : string,
+        public startTime : Date,
+        public finishTime : Date,
+        public totalTime : TimePeriod,
         public description : string) {
 
     }
